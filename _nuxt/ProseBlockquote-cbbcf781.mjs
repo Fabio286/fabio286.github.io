836c03beda8f15f1f34fd39c78a6d96507536494
Fabio286 <Fabio286@users.noreply.github.com>
2022-08-01 13:10:48 +0000

    deploy: 83311b16a3b921f68f63e2db8373796e6e771e88

---
+++ _nuxt/ProseBlockquote-cbbcf781.mjs
@@ -0,0 +1,1 @@
+import{l as o,o as r,m as t,r as c}from"./entry-7c0e8b7b.mjs";const l={};function s(e,a){return r(),t("blockquote",null,[c(e.$slots,"default")])}var f=o(l,[["render",s]]);export{f as default};
---
+++ _nuxt/ProseTbody-9d3b9774.mjs
@@ -0,0 +1,1 @@
+import{l as r,o,m as t,r as s}from"./entry-7c0e8b7b.mjs";const a={};function n(e,c){return o(),t("tbody",null,[s(e.$slots,"default")])}var d=r(a,[["render",n]]);export{d as default};
